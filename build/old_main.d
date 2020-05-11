5ce58dbf020eb7aef70e764cb1722002bc8db109
fid <gbushta@sonic.net>
2020-05-11 15:25:02 -0700

    add files to repo

---
+++ build/old_main.d
@@ -0,0 +1,14 @@
+/Users/fid/badge/hadbadge2019_workshops/basic/app-GOL4/build/old_main.o: \
+ /Users/fid/badge/hadbadge2019_workshops/basic/app-GOL4/old_main.c \
+ ../fpgasoc/apps-sdk/../soc/ipl/gloss/mach_defines.h \
+ ../fpgasoc/apps-sdk/sdk.h \
+ ../fpgasoc/apps-sdk/../soc/ipl/syscallable/gfx_load.h \
+ ../fpgasoc/apps-sdk/../soc/ipl/syscallable/cache.h
+
+../fpgasoc/apps-sdk/../soc/ipl/gloss/mach_defines.h:
+
+../fpgasoc/apps-sdk/sdk.h:
+
+../fpgasoc/apps-sdk/../soc/ipl/syscallable/gfx_load.h:
+
+../fpgasoc/apps-sdk/../soc/ipl/syscallable/cache.h:
